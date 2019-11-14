8b91161e69a5c522dc767fb2dd29515c0801fb39
Juan C Moreno <jcrogel@gmail.com>
2019-11-13 21:45:02 -0800

    Iniital commit

---
+++ src/main/java/application/Main.java
@@ -0,0 +1,71 @@
+package application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+import com.fxgraph.graph.CellType;
+import com.fxgraph.graph.Graph;
+import com.fxgraph.graph.Model;
+import com.fxgraph.layout.base.Layout;
+import com.fxgraph.graph.com.fxgraph.layout.random.RandomLayout;
+
+import java.net.URL;
+
+public class Main extends Application {
+
+    Graph graph = new Graph();
+
+    @Override
+    public void start(Stage primaryStage) {
+        BorderPane root = new BorderPane();
+
+        graph = new Graph();
+
+        root.setCenter(graph.getScrollPane());
+
+        Scene scene = new Scene(root, 1024, 768);
+        URL res = getClass().getResource("../application.css");
+        scene.getStylesheets().add(res.toExternalForm());
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+
+        addGraphComponents();
+
+        Layout layout = new RandomLayout(graph);
+        layout.execute();
+
+    }
+
+    private void addGraphComponents() {
+
+        Model model = graph.getModel();
+
+        graph.beginUpdate();
+
+        model.addCell("Cell A", CellType.RECTANGLE);
+        model.addCell("Cell B", CellType.RECTANGLE);
+        model.addCell("Cell C", CellType.RECTANGLE);
+        model.addCell("Cell D", CellType.TRIANGLE);
+        model.addCell("Cell E", CellType.TRIANGLE);
+        model.addCell("Cell F", CellType.RECTANGLE);
+        model.addCell("Cell G", CellType.RECTANGLE);
+/*
+        model.addEdge("Cell A", "Cell B");
+        model.addEdge("Cell A", "Cell C");
+        model.addEdge("Cell B", "Cell C");
+        model.addEdge("Cell C", "Cell D");
+        model.addEdge("Cell B", "Cell E");
+        model.addEdge("Cell D", "Cell F");
+        model.addEdge("Cell D", "Cell G");
+*/
+        graph.endUpdate();
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
